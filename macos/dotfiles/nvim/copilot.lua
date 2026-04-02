bc3c34018e8db725e845a0be6f5ecd264fddc606
James Cheney <jamesdcheney0@gmail.com>
2026-04-02 07:31:33 -0700

    try to get copilot in lua

--- macos/dotfiles/nvim/copilot.lua
+++ macos/dotfiles/nvim/copilot.lua
@@ -1,45 +1,20 @@
 return {
-	{
-		"echasnovski/snacks.nvim",
-		event = "VeryLazy",
-		config = true,
-	},
-	{
-		"zbirenbaum/copilot.lua",
-		cmd = "Copilot",
-		event = "InsertEnter",
-		config = function()
-			require("copilot").setup({})
-			local Snacks = require("snacks")
-			local copilot_exists = pcall(require, "copilot")
-
-			if copilot_exists then
-				Snacks.toggle({
-					name = "Copilot Completion",
-					color = {
-						enabled = "azure",
-						disabled = "orange",
-					},
-					get = function()
-						return not require("copilot.client").is_disabled()
-					end,
-					set = function(state)
-						if state then
-							require("copilot.command").enable()
-						else
-							require("copilot.command").disable()
-						end
-					end,
-				}):map("<leader>at")
-			end
-		end,
-	},
-	{
-		"CopilotC-Nvim/CopilotChat.nvim",
-		event = "VeryLazy",
-		dependencies = { "zbirenbaum/copilot.lua" },
-		config = function()
-			require("CopilotChat").setup({})
-		end,
-	},
+	"github/copilot.vim",
+	config = function()
+		vim.g.copilot_filetypes = {
+			markdown = false,
+			yaml = false,
+		}
+	end,
 }
+
+vim.api.nvim_create_autocmd("FileType", {
+  pattern = "markdown",
+  callback = function()
+    local cmp_ok, cmp = pcall(require, "cmp")
+    if cmp_ok then
+      cmp.setup.buffer { enabled = false }
+    end
+  end,
+})
+
